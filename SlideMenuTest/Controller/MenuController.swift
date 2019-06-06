c785f0e7f8c968ad07f8c01d625468f8f959123e
Usama Minegishi <usama@sonarism.com>
2019-06-07 06:00:52 +0900

    [add] table view as menu

--- SlideMenuTest/Controller/MenuController.swift
+++ SlideMenuTest/Controller/MenuController.swift
@@ -8,14 +8,51 @@
 
 import UIKit
 
+private let reuseIdentifer = "MenuOpetionCell"
+
 class MenuController: UIViewController {
     // MARK: - Proparties
+    
+    var tableView: UITableView!
     
     // MARK: - Init
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
     }
     
     // MARK: - Handlers
+    
+    func configureTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(MenuOptionCell.self, forCellReuseIdentifier: reuseIdentifer)
+        
+        tableView.backgroundColor = .darkGray
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 80
+        
+        view.addSubview(tableView)
+        
+        // constraints
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    }
+}
+
+extension MenuController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifer, for: indexPath) as! MenuOptionCell
+        return cell
+    }
 }
---
+++ SlideMenuTest/View/MenuOptionCell.swift
@@ -0,0 +1,55 @@
+//
+//  MenuOptionCell.swift
+//  SlideMenuTest
+//
+//  Created by Usama Minegishi on 2019/06/07.
+//  Copyright © 2019 Usama Minegishi. All rights reserved.
+//
+
+import UIKit
+
+class MenuOptionCell: UITableViewCell {
+
+    // MARK: - Properties
+    let iconImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.backgroundColor = .blue
+        return iv
+    } ()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Sample text"
+        
+        return label
+    } ()
+    
+    // MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .darkGray
+        
+        addSubview(iconImageView)
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        iconImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 12 + 80).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        descriptionLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 92 + 36).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: - Handlers
+
+}
